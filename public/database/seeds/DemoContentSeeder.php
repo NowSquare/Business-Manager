93caa970d28428f69f950999ce4a21cdbbf56e5f
Business Manager <support@businessmanager.app>
2019-02-23 09:07:03 +0100

    Added invoice demo seeder

--- public/database/seeds/DemoContentSeeder.php
+++ public/database/seeds/DemoContentSeeder.php
@@ -21,6 +21,10 @@ class DemoContentSeeder extends Seeder
         $user_count = 76;
         $company_count = 28;
         $project_count = 16;
+        $invoice_count = 28;
+        $invoice_start_number = 260;
+        $invoice_item_count = [5, 12];
+        $invoice_discount_count = [1, 1];
 
         $faker = Faker::create();
 
@@ -381,6 +385,140 @@ class DemoContentSeeder extends Seeder
 							$project_proposition_item->updated_by = 1;
 							$project_proposition_item->updated_at = $updated_at;
 							$project_proposition_item->save();
+						}
+          }
+        }
+
+        if ($invoice_count > 0) {
+          foreach (range(1, $invoice_count) as $invoice_index) {
+						$reference = $invoice_start_number;
+            $invoice_start_number++;
+
+            $item_count = mt_rand($invoice_item_count[0], $invoice_item_count[1]);
+            $discount_count = mt_rand($invoice_discount_count[0], $invoice_discount_count[1]);
+
+						$tax_rates = [1900, 2000, 2100];
+
+						// Totals
+						$total = 0;
+						$total_discount = 0;
+						$total_tax = 0;
+
+						// Random tax rate
+						$tax_rate = $tax_rates[mt_rand(0, count($tax_rates) - 1)];
+						$tax = $tax_rate / 100;
+
+						// Invoice items
+						$invoice_items = [];
+            $invoice_item_quantity = [5, 12];
+            $invoice_item_unit_price = [4000, 4500, 4800, 7000, 8000, 9000];
+            $invoice_discount_quantity = [10, 15, 20];
+
+						foreach (range(1, $item_count) as $index) {
+							$quantity = mt_rand($invoice_item_quantity[0], $invoice_item_quantity[1]);
+							$unit_price = $invoice_item_unit_price[mt_rand(0, count($invoice_item_unit_price) - 1)];
+							$total_without_tax = $quantity * $unit_price;
+
+							$total += $total_without_tax;
+							$total_tax += ($total_without_tax * $tax) / 100;
+
+							$invoice_items[] = [
+                'type' => 'item', 
+                'description' => ucfirst($faker->bs), 
+                'quantity' => $quantity, 
+                'unit' => 'hour', 
+                'unit_price' => $unit_price, 
+                'discount_type' => null
+              ];
+						}
+
+						$total_items = $total;
+
+						foreach (range(1, $discount_count) as $index) {
+							$quantity = $invoice_discount_quantity[mt_rand(0, count($invoice_discount_quantity) - 1)];
+
+              // Always use %
+              $total_without_tax = ($total_items / 100) * $quantity;
+
+							$total -= $total_without_tax;
+							$total_tax -= ($total_without_tax * $tax) / 100;
+							$total_discount += $total_without_tax;
+
+							$invoice_items[] = [
+                'type' => 'discount', 
+                'description' => ucfirst($faker->bs), 
+                'quantity' => $quantity, 
+                'discount_type' => '%', 
+                'unit' => null, 
+                'unit_price' => null
+              ];
+						}
+
+            $total_tax = round($total_tax);
+
+            $days_ago = (($invoice_count * 2) - ($invoice_index * 2));
+
+						$created_at = $faker->dateTimeBetween($startDate = '-1 months', $endDate = 'now');
+						$updated_at = $faker->dateTimeBetween($startDate = $created_at, $endDate = 'now');
+
+            $issue_date = $faker->dateTimeBetween($startDate = '-' . $days_ago . ' days', $endDate = '-' . $days_ago . ' days');
+            $due_date = $faker->dateTimeBetween($startDate = '-' . ($days_ago - 14) . ' days', $endDate = '-' . ($days_ago - 14) . ' days');
+
+            $sent_date = null;
+						$paid_date = null;
+						$partially_paid_date = null;
+						$written_off_date = null;
+
+            // Only different status for last invoices
+            if ($invoice_index >= $invoice_count - 2) {
+              // Draft
+            } elseif ($invoice_index >= $invoice_count - 5) {
+              $sent_date = $faker->dateTimeBetween($startDate = '-' . $days_ago . ' days', $endDate = '-' . $days_ago . ' days');
+            } else {
+              $paid_within_days = mt_rand(5, 14);
+              $sent_date = $faker->dateTimeBetween($startDate = '-' . $days_ago . ' days', $endDate = '-' . $days_ago . ' days');
+              $paid_date = $faker->dateTimeBetween($startDate = '-' . ($days_ago - $paid_within_days) . ' days', $endDate = '-' . ($days_ago - $paid_within_days) . ' days');
+            }
+
+						$invoice = new \Platform\Models\Invoice;
+						$invoice->account_id = 1;
+						$invoice->company_id = mt_rand(2, $company_count - 1);
+						$invoice->reference = $reference;
+						$invoice->currency_code = 'USD';
+						$invoice->total = $total;
+						$invoice->total_discount = $total_discount;
+						$invoice->total_tax = $total_tax;
+
+						$invoice->issue_date = $issue_date;
+						$invoice->due_date = $due_date;
+						$invoice->sent_date = $sent_date;
+						$invoice->paid_date = $paid_date;
+						$invoice->partially_paid_date = $partially_paid_date;
+						$invoice->written_off_date = $written_off_date;
+  
+						$invoice->created_by = 1;
+						$invoice->created_at = $created_at;
+						$invoice->updated_by = 1;
+						$invoice->updated_at = $updated_at;
+						$invoice->save();
+
+						// Items
+						foreach ($invoice_items as $item) {
+							$invoice_item = new \Platform\Models\InvoiceItem;
+							$invoice_item->invoice_id = $invoice->id;
+							$invoice_item->type = $item['type'];
+							$invoice_item->description = $item['description'];
+							$invoice_item->quantity = $item['quantity'] * 100;
+							$invoice_item->unit = $item['unit'];
+							$invoice_item->discount_type = $item['discount_type'];
+							$invoice_item->unit_price = $item['unit_price'];
+							$invoice_item->tax_rate = $tax_rate;
+							$invoice_item->type = $item['type'];
+							$invoice_item->created_by = 1;
+							$invoice_item->created_at = $created_at;
+							$invoice_item->updated_by = 1;
+							$invoice_item->updated_at = $updated_at;
+							$invoice_item->save();
 						}
           }
         }
