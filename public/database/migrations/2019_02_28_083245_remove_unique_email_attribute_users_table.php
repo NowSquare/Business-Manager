aaeafd77df2412b34aa2bdd9ad6726c05cea3119
Business Manager <support@businessmanager.app>
2019-03-01 07:44:45 +0100

    Removed restore unique index on migration reset because of error

--- public/database/migrations/2019_02_28_083245_remove_unique_email_attribute_users_table.php
+++ public/database/migrations/2019_02_28_083245_remove_unique_email_attribute_users_table.php
@@ -26,7 +26,7 @@ class RemoveUniqueEmailAttributeUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unique('email');
+            //$table->unique('email');
         });
     }
 }
