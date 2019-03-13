911ae28dccb0ddac647e72e62611a52d6806533c
Business Manager <support@businessmanager.app>
2019-03-13 08:48:24 +0100

    Added QR library classes to config

--- public/config/app.php
+++ public/config/app.php
@@ -187,6 +187,7 @@ return [
         Maatwebsite\Excel\ExcelServiceProvider::class,
         Barryvdh\Elfinder\ElfinderServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
+        Milon\Barcode\BarcodeServiceProvider::class,
 
     ],
 
@@ -240,6 +241,8 @@ return [
         'Countries' => Monarobase\CountryList\CountryListFacade::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
+        'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
+        'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class,
     ],
 
 ];
