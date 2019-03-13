b02e7b6cb7fc96dd19492697f3a53ed797b6f828
Business Manager <support@businessmanager.app>
2019-03-13 10:10:17 +0100

    First check if Pusher config exists in .env file

--- public/app/Providers/AppServiceProvider.php
+++ public/app/Providers/AppServiceProvider.php
@@ -36,18 +36,32 @@ class AppServiceProvider extends ServiceProvider
         }
 
         // Pusher
-        $pusher_configured = false;
-        $pusher_app_id = Core\Settings::get('pusher_app_id', 'string', null);
-        $pusher_key = Core\Settings::get('pusher_key', 'string', null);
-        $pusher_secret = Core\Settings::get('pusher_secret', 'string', null);
-        $pusher_cluster = Core\Settings::get('pusher_cluster', 'string', null);
-
-        if ($pusher_app_id !== null && $pusher_key !== null && $pusher_secret !== null && $pusher_cluster !== null) {
+        if (
+          config('broadcasting.connections.pusher.app_id') != '' && 
+          config('broadcasting.connections.pusher.key') != '' && 
+          config('broadcasting.connections.pusher.secret') != '' && 
+          config('broadcasting.connections.pusher.options.cluster') != ''
+        ) {
           $pusher_configured = true;
-          config(['broadcasting.connections.pusher.app_id' => $pusher_app_id]);
-          config(['broadcasting.connections.pusher.key' => $pusher_key]);
-          config(['broadcasting.connections.pusher.secret' => $pusher_secret]);
-          config(['broadcasting.connections.pusher.options.cluster' => $pusher_cluster]);
+        } else {
+          $pusher_configured = false;
+          $pusher_app_id = Core\Settings::get('pusher_app_id', 'string', null);
+          $pusher_key = Core\Settings::get('pusher_key', 'string', null);
+          $pusher_secret = Core\Settings::get('pusher_secret', 'string', null);
+          $pusher_cluster = Core\Settings::get('pusher_cluster', 'string', null);
+
+          if (
+            $pusher_app_id !== null && 
+            $pusher_key !== null && 
+            $pusher_secret !== null && 
+            $pusher_cluster !== null
+          ) {
+            $pusher_configured = true;
+            config(['broadcasting.connections.pusher.app_id' => $pusher_app_id]);
+            config(['broadcasting.connections.pusher.key' => $pusher_key]);
+            config(['broadcasting.connections.pusher.secret' => $pusher_secret]);
+            config(['broadcasting.connections.pusher.options.cluster' => $pusher_cluster]);
+          }
         }
 
         view()->share('pusher_configured', $pusher_configured);
